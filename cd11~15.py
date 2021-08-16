5ea6607a4c013625aa54aae9f3ec12b570cb813b
hoonek <karlhkim1@naver.com>
2021-08-16 20:54:30 +0900

    codeup q20 solution

---
+++ cd11~15.py
@@ -0,0 +1,23 @@
+'''
+c = float(input())
+print(c)
+
+a = int(input())
+b = int(input())
+
+print(a)
+print(b)
+
+d = input()
+e = input()
+
+print(d)
+print(e)
+
+a = float(input())
+print(a)
+print(a)
+print(a)
+'''
+a, b = input().split()
+print(a, b)
---
+++ cd6~10.py
@@ -0,0 +1,9 @@
+print("\"!@#$%^&*()'")
+print('"C:\Download\\\'hello\'.py"')
+print('"Hello\\nWorld"')
+
+a = input()
+print(a)
+
+a = int(input())
+print(a)
