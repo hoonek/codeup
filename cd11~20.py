182a0d03fc80b5e7438326d109a9e6eaf103de1e
hoonek <karlhkim1@naver.com>
2021-08-18 20:05:18 +0900

    codeup q40 sol

---
+++ cd11~20.py
@@ -0,0 +1,41 @@
+'''
+c = float(input())
+print(c)
+
+a = int(input())
+b = int(input())
+
+print(a)
+print(b)
+
+d = input()
+e = input()
+
+print(d)
+print(e)
+
+a = float(input())
+print(a)
+print(a)
+print(a)
+
+a, b = input().split()
+print(a, b)
+
+a, b = input().split()
+
+print(b, a)
+
+a = input()
+
+print(a, a, a)
+
+a, b = input().split(':')
+print(a, b, sep=':')
+
+y, m, d = input().split('.')
+print(d+"-"+m+"-"+y)
+
+a, b = input().split('-')
+print(a+''+b)
+'''
---
+++ cd31~40.py
@@ -0,0 +1,45 @@
+'''
+c = int(input())
+print(chr(c))  #c에 저장되어 있는 정수 값을 유니코드 문자(chracter)로 바꿔 출력한다.
+#ord 문자->숫자 / chr 숫자->문자
+
+n = int(input())
+print(-1 * n)
+
+a = ord(input())
+b = a + 1
+print(chr(b))
+
+a, b = split().input()
+c = a - b
+print(c)
+
+a, b = input().split()
+c = int(a) - int(b)
+print(c)
+
+f1, f2 = input().split()
+m = float(f1) * float(f2)
+print(m)
+
+w, n = input().split()
+print(w*int(n))
+
+n = input()
+s = input()
+print(int(n)*s)
+
+a,b = input().split()
+c = int(a)**int(b)
+print(c)
+
+a,b = input().split()
+c = float(a)**float(b)
+print(c)
+
+a,b = input().split()
+c = int(a)//int(b)
+print(c)
+
+'''
+
